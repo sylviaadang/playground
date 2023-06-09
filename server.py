ec942822bb87b4f1b6e5d4d9f566553a1bec8d30
Your Name <email@example.com>
2023-06-09 13:20:45 -0700

    update

--- server.py
+++ server.py
@@ -2,18 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# def welcome():
+#     return render_template('index3.html', repeat=3, color='blue')
+
+
+# def more(num):
+#     repeat = num
+#     return render_template('index3.html', repeat=repeat, color='blue')
 @app.route('/play')
-def welcome():
-    return render_template('index.html')
-
-
 @app.route('/play/<int:num>')
-def more(num):
-    repeat = num
-    return render_template('index2.html', repeat=repeat)
-
 @app.route('/play/<int:num>/<color>')
-def color(num, color):
+def color(num=3, color='blue'):
     repeat = num
     color = color
     return render_template('index3.html', repeat=repeat, color=color)
